f999cd10f550fd01ba2abc569451b2a0052ec467
zampierilucas <lcasmz54@gmail.com>
2021-02-23 13:39:54 -0300

    Added Pyinstaller tmp path

--- src/HeadsetControl_SystemTray.py
+++ src/HeadsetControl_SystemTray.py
@@ -5,6 +5,7 @@ import tempfile
 import webbrowser
 import subprocess
 import os
+import sys
 import pkg_resources
 from infi.systray import SysTrayIcon
 from PIL import Image, ImageDraw, ImageFont
@@ -24,6 +25,14 @@ def on_quit_callback(systray):
     global main_loop
     main_loop = False
 
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.getcwd()
+
+    print(base_path)
+    return base_path + relative_path
 
 def headset_status():
     global r, g, b
@@ -31,7 +40,7 @@ def headset_status():
     global font_type
 
     # Get headset data
-    output = subprocess.check_output(os.getcwd()+'\lib\headsetcontrol -bc', shell=True, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL) or False
+    output = subprocess.check_output(resource_path('\lib\headsetcontrol') + ' -bc', shell=True, stdin=subprocess.DEVNULL, stderr=subprocess.DEVNULL) or False
 
     # Not connected
     if not output:
